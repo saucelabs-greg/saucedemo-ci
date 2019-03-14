25fdd952c23e6a5a4b8a8b9357510f8a1746b1c5
Alex Griffen <radhack242@gmail.com>
2019-03-14 14:18:14 -0700

    add IE back in so I get failures, cause IE...

--- src/test/java/com/swaglabs/Tests/TestBase.java
+++ src/test/java/com/swaglabs/Tests/TestBase.java
@@ -61,7 +61,7 @@ public class TestBase {
             new Object[]{"MicrosoftEdge", "latest-1", "Windows 10"},
             new Object[]{"firefox", "latest", "Windows 10"},
             new Object[]{"firefox", "latest-1", "Windows 10"},
-            //                new Object[]{"internet explorer", "11.0", "Windows 7"},
+            new Object[]{"internet explorer", "11.0", "Windows 7"},
 //            new Object[]{"safari", "latest", "OS X 10.11"},
 //            new Object[]{"safari", "latest-1", "OS X 10.11"},
             new Object[]{"chrome", "latest", "OS X 10.10"},
