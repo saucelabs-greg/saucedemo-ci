30d76b4b1b1cca018f4d7ad94a0076eac12769aa
Alex Griffen <radhack242@gmail.com>
2019-03-15 09:00:10 -0700

    add more information to get Jenkins reporting up

--- src/test/java/com/swaglabs/Tests/TestBase.java
+++ src/test/java/com/swaglabs/Tests/TestBase.java
@@ -134,7 +134,7 @@ public class TestBase {
         String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
         sessionId.set(id);
         String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
-                id, "some job name");
+                sessionId, System.getenv("JOB_NAME"));
         System.out.println(message);
     }
 
